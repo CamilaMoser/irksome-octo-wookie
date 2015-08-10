423b4fd1f2f32b489f68f432628ceafab350ea84
unknown <10067320@W1849397.portoalegre.pucrsnet.br>
2015-08-10 20:53:51 -0300

    Matrizes - Aula 04

---
+++ src/pucrs/lapro3lm/matrizes/Exercicio02.java
@@ -0,0 +1,25 @@
+package pucrs.lapro3lm.matrizes;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		int[][] a = { { 1, 0 }, { 0, 2 } };
+		int[][] b = { { 0, 1 }, { 1, 2 } };
+		int[][] c = { { 0, 1 }, { 2, 0 } };
+		int[][] r;
+		
+		r = multiplicar(a, b);
+	}
+
+	private static int[][] multiplicar(int[][] a, int[][] b) {
+		if (b.length != a[0].length)
+			throw new RuntimeException("Numero de linhas de b deve coincidir com o numero de colunas de a.");
+		
+		int[][] r = new int[a.length][b[0].length];
+		
+		// TODO: calcular r
+		
+		return r;
+	}
+
+}
