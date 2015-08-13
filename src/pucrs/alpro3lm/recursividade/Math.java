d114c56fb24b24a9ebd196667d35e89a6e839e23
Marco Mangan <marco.mangan@pucrs.br>
2015-08-12 21:00:03 -0300

    recursividade

---
+++ src/pucrs/alpro3lm/recursividade/Math.java
@@ -0,0 +1,79 @@
+package pucrs.alpro3lm.recursividade;
+
+public class Math {
+
+	
+	public static void main(String[] args) {
+		System.out.println(Math.factorial(3));
+	}
+
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public static int factorial(int n) {
+		if (n < 0)
+			throw new IllegalArgumentException();
+		
+		return fact(n);
+	}
+
+	/*
+	 * 
+	 */
+	private static int fact(int n) {
+		if (n == 0)
+			return 1;
+		else
+		    return n * fact(n - 1);
+	}
+	
+	
+	public static int sum(int n) {
+		if (n < 0)
+			throw new IllegalArgumentException();
+		
+		return s(n);
+	}	
+	
+	private static int s(int n) {
+		if (n == 0)
+			return 0;
+		
+		return n + s(n - 1);
+	}
+	
+	public static boolean isPalindrome(String s) {
+		if (s == null)
+			throw new IllegalArgumentException();	
+		s = s.trim();
+		if (s.isEmpty())
+			throw new IllegalArgumentException();			
+		return p(s);
+	}
+	
+	
+	
+	private static boolean p(String s) {
+		if (s.equals(""))
+			return true;
+		if (s.length() == 1)
+			return true;		
+		
+		if (s.charAt(0) != s.charAt(s.length() - 1))
+			return false;
+		s = s.substring(1, s.length() - 2);
+		return p(s);
+	}
+	
+	
+	
+	
+	
+	
+
+	private Math() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/pucrs/alpro3lm/recursividade/MathTest.java
@@ -0,0 +1,29 @@
+package pucrs.alpro3lm.recursividade;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathTest {
+
+	@Test
+	public void testSumZero() {
+		int expected = 0;
+		int actual = Math.sum(0);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testSumOne() {
+		int expected = 1;
+		int actual = Math.sum(1);
+		assertEquals(expected, actual);
+	}	
+	
+	@Test
+	public void testSumTwo() {
+		int expected = 3;
+		int actual = Math.sum(2);
+		assertEquals(expected, actual);
+	}	
+}
