ce5fbc1225f071708bfdce48effddd99f49e40b9
Marco Mangan <marco.mangan@pucrs.br>
2015-11-18 20:03:56 -0200

    Leitura de arquivo

--- src/br/pucrs/alpro3lm/grafos/Grafo.java
+++ src/br/pucrs/alpro3lm/grafos/Grafo.java
@@ -1,8 +1,11 @@
 package br.pucrs.alpro3lm.grafos;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Grafo {
@@ -17,6 +20,29 @@ public class Grafo {
 		m = new int[nodos + 1][nodos + 1];
 	}
 
+	// TODO: ler XML ou JSON
+	// TODO: ler graphviz
+	// TODO: ver KML e GPX
+	public Grafo(String arquivo) throws IOException {
+		FileReader fr = new FileReader(arquivo);
+		BufferedReader br = new BufferedReader(fr);
+		String n = br.readLine();
+		// System.out.println(n);
+		int nodos = Integer.parseInt(n);
+		m = new int[nodos + 1][nodos + 1];
+
+		String par;
+		while ((par = br.readLine()) != null) {
+			// System.out.println(par);
+			String[] campos = par.split(" ");
+			int a = Integer.parseInt(campos[0]);
+			int b = Integer.parseInt(campos[1]);
+			adicionarArco(a, b);
+		}
+		br.close();
+		fr.close();
+	}
+
 	public void adicionarArco(int a, int b) {
 		adicionarArco(a, b, 1);
 	}
@@ -25,6 +51,9 @@ public class Grafo {
 		verificar(a);
 		verificar(b);
 		m[a][b] = v;
+		// m[b][a] = v;
+		// acrescentar aresta de retorno
+		// no caso de não-dirigido
 	}
 
 	private void verificar(int a) {
@@ -38,7 +67,7 @@ public class Grafo {
 		limpar();
 		Queue<Integer> q = new LinkedList<>();
 
-		//visitar(s); // 1. Visite um nodo arbitrário
+		// visitar(s); // 1. Visite um nodo arbitrário
 		r.add(s);
 		marcar(s); // 2. [Marque o nodo] e [coloque-o em uma fila Q]
 		q.add(s);
@@ -48,7 +77,7 @@ public class Grafo {
 			// 5. Para cada nodo M (não marcado) adjacente a N
 			for (int m : adjacentes(n)) {
 				if (!isMarcado(m)) {
-					//visitar(m); // 6. Visite M
+					// visitar(m); // 6. Visite M
 					r.add(m);
 					q.add(m); // 7. Coloque M na fila Q
 					marcar(m); // 8. Marque M
@@ -81,7 +110,7 @@ public class Grafo {
 	}
 
 	private void profundidade0(int s, List<Integer> r) {
-		//visitar(s);
+		// visitar(s);
 		r.add(s);
 		marcar(s);
 		for (int m : adjacentes(s)) {
